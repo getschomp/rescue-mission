4ed2c9fc961fbbce602c5195e9e5313e67549ed9
Allison <allisonmariebrowne@gmail.com>
2014-12-16 12:20:51 -0500

    new answer submission fully functional

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,15 +1,30 @@
 class AnswersController < ApplicationController
+  def current_user_id
+    1
+  end
+
   def new
+    @question = Question.find(params[:question_id])
     @answer = Answer.new
   end
 
   def create
-
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    @answer.user_id = current_user_id
+    @answer.question_id = @question.id
+    if @answer.save
+      redirect_to @question, notice: "Answer sucessfully created."
+    else
+      render :new
+    end
   end
 
-  def edit
-  end
 
   def destroy
+  end
+
+  def answer_params
+    params.require(:answer).permit(:answer)
   end
 end
